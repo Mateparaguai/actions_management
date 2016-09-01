ecf4a24a1e0dfbd47d6ad60323219d249fb2122a
Mateparaguai <peimate@yandex.ru>
2016-09-01 18:16:59 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -2,3 +2,11 @@
 import bpy
 for a in bpy.data.actions: 
     a.user_clear()
+
+# del all actions
+import bpy
+for a in bpy.data.actions:
+    if a.users == 1 and a.use_fake_user:
+        pass
+    else
+        a.user_clear()
