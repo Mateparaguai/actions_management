2f2a7e46f629436a157c361198e3ee781c22ef76
Mateparaguai <peimate@yandex.ru>
2016-09-01 19:24:13 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -9,7 +9,13 @@ for act in bpy.data.actions:
         a.user_clear()
     
 # print to consol fakes and not fakes users
+import bpy
 for act in bpy.data.actions:
     if act.use_fake_user == True: print('action name is - ', act.name, 'fake_user ', act.users, ' - fake_user')
     elif act.use_fake_user == False: print('action name is - ', act.name, 'fake_user ', act.users, ' - NOT fake_user')
     
+# del action by name
+import bpy
+for act in bpy.data.actions:
+    if act.name == 'test_CH01_RUN':
+        act.user_clear()
