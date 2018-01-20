32ce85a3eea983d332eb08ed483203b207de6e5e
Mateparaguai <peimate@yandex.ru>
2018-01-20 12:12:35 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -9,6 +9,14 @@ import bpy
 for a in bpy.data.actions: 
     print(a.name)
 
+    
+    
+    
+    
+    
+    
+    
+    
 # rename all animation, add 
 import bpy
 add_string = '#ADD' 
@@ -16,17 +24,6 @@ for a in bpy.data.actions:
     b = add_string + '_' + a.name
     a.name = b
 
-# offset all actions
-import bpy
-sel_objs = bpy.context.selected_objects
-n = 0
-for s in sel_objs:
-    action = s.animation_data.action
-    n += 1
-    for fcurve in action.fcurves :
-        for p in fcurve.keyframe_points :
-            p.co[0] += n
-
-            
+          
 # Select action in 3D viewport
 ...
